918acab1cd7ee04777f6264d2e91d08008bc6d28
jorozcove <jorozcove@unal.edu.co>
2024-03-30 14:20:31 -0500

    Handle webclient exceptions (Microservice call errors)

---
+++ src/main/java/leare/apiGateway/errors/CustomExceptionResolver.java
@@ -0,0 +1,63 @@
+package leare.apiGateway.errors;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// Importa las clases necesarias
+
+@Component
+public class CustomExceptionResolver extends DataFetcherExceptionResolverAdapter {
+
+    private final ObjectMapper objectMapper;
+
+    public CustomExceptionResolver(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable throwable, DataFetchingEnvironment env) {
+
+        // Si la excepción es de tipo WebClientResponseException (Error al consumir el microservicio)
+        if (throwable instanceof WebClientResponseException) {
+            WebClientResponseException ex = (WebClientResponseException) throwable;
+
+            // Crear un objeto de error personalizado para GraphQL (statusCode y responseBody)
+            Map<String, Object> errorMap = new HashMap<>();
+            errorMap.put("statusCode", ex.getStatusCode().value());
+            errorMap.put("responseBody", ex.getResponseBodyAsString());
+
+            try {
+                String errorMessage = objectMapper.writeValueAsString(errorMap);
+
+                return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.ExecutionAborted)
+                    .message(errorMessage)
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+
+            } catch (JsonProcessingException e) {
+                return super.resolveToSingleError(throwable, env);
+            }
+        }
+
+        return super.resolveToSingleError(throwable, env);
+    }
+}
+
